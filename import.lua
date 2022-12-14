a4f4816efc1b9c27bd3e54c4e63b0c000a986b57
Annalouu <112778590+Annalouu@users.noreply.github.com>
2022-12-14 11:04:33 +0100

    Update import.lua

--- import.lua
+++ import.lua
@@ -1,8 +1,10 @@
-if not IsDuplicityVersion() then -- Only register this event for the client
-	AddEventHandler('esx:setPlayerData', function(key, val, last)
-		if GetInvokingResource() == 'es_extended' then
-			ESX.PlayerData[key] = val
-			if OnPlayerData ~= nil then OnPlayerData(key, val, last) end
-		end
-	end)
+if Config.framework == 'esx' then
+	if not IsDuplicityVersion() then -- Only register this event for the client
+		AddEventHandler('esx:setPlayerData', function(key, val, last)
+			if GetInvokingResource() == 'es_extended' then
+				ESX.PlayerData[key] = val
+				if OnPlayerData ~= nil then OnPlayerData(key, val, last) end
+			end
+		end)
+	end
 end
